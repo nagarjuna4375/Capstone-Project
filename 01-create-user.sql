deb47eaeed230c1158aa7f528b8c68bbe2877b7d
nagarjuna4375 <114644141+nagarjuna4375@users.noreply.github.com>
2022-10-21 00:40:01 -0700

    Add files via upload

---
+++ 01-create-user.sql
@@ -0,0 +1,6 @@
+CREATE USER 'Nagarjuna'@'localhost' IDENTIFIED BY 'Vinnu@4375';
+
+GRANT ALL PRIVILEGES ON * . * TO 'Nagarjuna'@'localhost';
+
+
+ALTER USER 'Nagarjuna'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Vinnu@4375';
